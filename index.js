caa27d21c9b655c62ebe9e6a1a4eb7b27cb85b58
NowWithVitaminR <kristofer.kass@gmail.com>
2024-12-24 15:36:41 -0500

    small changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 let DefaultPage = "index.ejs";
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
