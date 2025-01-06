845ebbccd96e9b664686c283002ef9e03e3259ff
NowWithVitaminR <kristofer.kass@gmail.com>
2025-01-06 15:09:38 -0500

    fixed accordion menu to only let one menu be open at a time

--- public/js/menu.js
+++ public/js/menu.js
@@ -10,11 +10,10 @@ function getState(id) {
 
 // Accordion functionality
 var acc = document.getElementsByClassName("accordion");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-    var panel = acc[i].nextElementSibling;
-    var id = acc[i].getAttribute("data-id");
+for (let i = 0; i < acc.length; i++) {
+    let panel = acc[i].nextElementSibling;
+    let id = acc[i].getAttribute("data-id");
 
     // Retrieve saved state and apply it
     if (getState(id) === "open") {
@@ -27,8 +26,18 @@ for (i = 0; i < acc.length; i++) {
 
     // Add click event listener
     acc[i].addEventListener("click", function () {
-        var panel = this.nextElementSibling;
-        var id = this.getAttribute("data-id");
+        // Collapse all other panels
+        for (let j = 0; j < acc.length; j++) {
+            if (j !== i) {
+                acc[j].classList.remove("active");
+                acc[j].nextElementSibling.style.display = "none";
+                saveState(acc[j].getAttribute("data-id"), false); // Save state as closed
+            }
+        }
+
+        // Toggle the current panel
+        let panel = this.nextElementSibling;
+        let id = this.getAttribute("data-id");
         this.classList.toggle("active");
 
         if (panel.style.display === "block") {
